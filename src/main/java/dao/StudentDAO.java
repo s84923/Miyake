25e6be4486c292c3183fe43b83c964009f987108
suzaku <suzaku@LAPTOP-IF82MNII>
2024-04-15 12:10:32 +0900

    DAO

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -1,11 +1,34 @@
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
 package dao;
 
-public class StudentDAO {
-	private String baseSQL;
-	
-	
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import bean.Customer;
+public class StudentDAO extends DAO {
+
+	public Studdent search(String login,String password) 
+			throws Exception { 
+		Student student=null; 
+
+		Connection con=getConnection(); 
+		
+		PreparedStatement st;
+		st=con.prepareStatement(
+			"select * from customer where login=? and password=?");
+		st.setString(1,login);
+		st.setString(2,password);
+		ResultSet rs=st.executeQuery();
+
+		while (rs.next()) {
+			customer=new Customer();
+			customer.setId(rs.getInt("id"));
+			customer.setLogin(rs.getString("login"));
+			customer.setPassword(rs.getString("password"));
+		}
+
+		st.close();
+		con.close(); 
+		return customer;
+	}
 }
