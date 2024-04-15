8d511f2c517f48fbcca145e7f4d8323459b79494
suzaku <suzaku@LAPTOP-IF82MNII>
2024-04-15 12:39:48 +0900

    コメント

--- src/main/java/bean/Teacher.java
+++ src/main/java/bean/Teacher.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package bean;
 
 	public class Teacher {
@@ -32,37 +31,3 @@ package bean;
 				this.school=school;
 		}	
 }
-=======
-package bean; 
-public class Teacher implements java.io.Serializable {
-		private String id;
-		private String password;
-		private String name;
-		private String school;
-		
-		public String getId() {
-			return id;
-	    }
-		public void setId(String id) {
-				this.id=id;
-		}
-		public String getPassword() {
-			return password;
-	    }
-		public void setPassword(String password) {
-				this.password=password;
-		}
-		public String getName() {
-			return name;
-	    }
-		public void setName(String name) {
-				this.name=name;
-		}
-		public String getSchool() {
-			return school;
-	    }
-		public void setSchool(String school) {
-				this.school=school;
-		}	
-}
->>>>>>> branch 'master' of https://github.com/s84923/Miyake.git
