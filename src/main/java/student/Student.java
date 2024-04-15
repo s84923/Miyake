77763555804e84d09cde512bcdc16a35a4fcb397
suzaku <suzaku@LAPTOP-IF82MNII>
2024-04-15 11:09:44 +0900

    a

--- src/main/java/student/Student.java
+++ src/main/java/student/Student.java
@@ -1,4 +1,36 @@
 
 public class Student {
 
+		private String no;
+		private String name;
+		private int entYear;
+		private String dassNum;
+		private boolean isAttend;
+		private School school;
+		
+		public String getNo() {
+			return no;
+	    }
+		public void setNo(String no) {
+				this.no=no;
+		}
+		public String getName() {
+			return name;
+	    }
+		public void setEntYear(String ent) {
+				this.password=password;
+		}
+		public String getName() {
+			return name;
+	    }
+		public void setName(String name) {
+				this.name=name;
+		}
+		public School getSchool() {
+			return school;
+	    }
+		public void setSchool(String School) {
+				this.school=school;
+		}		
+}
 }
