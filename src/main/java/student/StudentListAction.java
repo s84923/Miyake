36eb3634ec50f82f115fa14da1b3ed86eab8a7f6
suzaku <suzaku@LAPTOP-IF82MNII>
2024-04-15 11:50:28 +0900

    エクション出来たよ

--- src/main/java/student/StudentListAction.java
+++ src/main/java/student/StudentListAction.java
@@ -1,5 +1,10 @@
 package student;
 
-public class StudentListAction {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+	public abstract class StudentListAction{
+		public abstract String execute(
+				HttpServletRequest request,HttpServletResponse response
+				)throws Exception;
 }
