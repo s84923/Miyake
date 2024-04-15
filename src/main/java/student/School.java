b3ce0bcfe557c84352814b4050abd09b134a6e56
suzaku <suzaku@LAPTOP-IF82MNII>
2024-04-15 11:45:47 +0900

    school 出来たよ

--- src/main/java/student/School.java
+++ src/main/java/student/School.java
@@ -1,4 +1,18 @@
-
+package student;
 public class School {
-
+	private String cd;
+	private String name;
+	
+	public String getCd() {
+		return cd;
+    }
+	public void setCd(String cd) {
+			this.cd=cd;
+	}
+	public String getName() {
+		return name;
+    }
+	public void setName(String name) {
+			this.name=name;
+	}
 }
