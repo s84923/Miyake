fdec734bd3355bd327764cb44ae3ba206ce7751b
s84923 <you.sou1108@gmail.com>
2024-04-24 10:25:29 +0900

    a

--- src/main/java/dao/CustomerDAO.java
+++ src/main/java/dao/CustomerDAO.java
@@ -5,30 +5,27 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import bean.Customer;
+
 public class CustomerDAO extends DAO {
 
-	public Customer search(String login,String password) 
-			throws Exception { 
-		Customer customer=null; 
+    public Customer search(String login, String password) throws Exception {
+        Customer customer = null;
 
-		Connection con=getConnection(); 
-		
-		PreparedStatement st;
-		st=con.prepareStatement(
-			"select * from customer where login=? and password=?");
-		st.setString(1,login);
-		st.setString(2,password);
-		ResultSet rs=st.executeQuery();
+        try (Connection con = getConnection();
+             PreparedStatement st = con.prepareStatement(
+                     "SELECT * FROM customer WHERE login=? AND password=?")) {
+            st.setString(1, login);
+            st.setString(2, password);
+            ResultSet rs = st.executeQuery();
 
-		while (rs.next()) {
-			customer=new Customer();
-			customer.setId(rs.getInt("id"));
-			customer.setLogin(rs.getString("login"));
-			customer.setPassword(rs.getString("password"));
-		}
+            if (rs.next()) {
+                customer = new Customer();
+                customer.setId(rs.getInt("id"));
+                customer.setLogin(rs.getString("login"));
+                customer.setPassword(rs.getString("password"));
+            }
+        }
 
-		st.close();
-		con.close(); 
-		return customer;
-	}
+        return customer;
+    }
 }
