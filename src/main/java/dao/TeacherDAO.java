03389914f7ee13adef37a30ae306e104f413afd4
s84923 <you.sou1108@gmail.com>
2024-04-24 12:31:17 +0900

    a

--- src/main/java/dao/TeacherDAO.java
+++ src/main/java/dao/TeacherDAO.java
@@ -1,18 +1,59 @@
-package dao;
-
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import bean.Teacher;
 
-public class TeacherDAO extends DAO{
-		
-		public Connection get(String id) throws Exception{
-			if (ds==null) {
-				InitialContext ic=new InitialContext();
-				ds=(DataSource)ic.lookup("java:/comp/env/jdbc/zaiko");
-		
-		}
-		return ds.getConnection();
-		}
+public class TeacherDAO extends DAO {
+
+    public Teacher get(String login, String password) throws Exception {
+        Teacher teacher = null;
+
+        Connection con = getConnection();
+
+        PreparedStatement st;
+        st = con.prepareStatement(
+                "SELECT * FROM teacher WHERE login=? AND password=?");
+        st.setString(1, login);
+        st.setString(2, password);
+        ResultSet rs = st.executeQuery();
+
+        if (rs.next()) {
+            teacher = new Teacher();
+            teacher.setId(rs.getString("id"));
+            teacher.setLogin(rs.getString("login"));
+            teacher.setPassword(rs.getString("password"));
+
+        }
+
+        rs.close(); 
+        st.close();
+        con.close();
+        return teacher;
+    }
+    public Teacher login(String id, String password) throws Exception {
+        Teacher teacher = null;
+
+        Connection con = getConnection();
+
+        PreparedStatement st;
+        st = con.prepareStatement(
+                "SELECT * FROM teacher WHERE id=? AND password=?");
+        st.setString(1, id);
+        st.setString(2, password);
+        ResultSet rs = st.executeQuery();
+
+        if (rs.next()) {
+            teacher = new Teacher();
+            teacher.setId(rs.getString("id"));
+            teacher.setLogin(rs.getString("login"));
+            teacher.setPassword(rs.getString("password"));
+            // ここで他の教師の情報をセットする場合、セットしてください
+        }
+
+        rs.close(); // ResultSetを閉じる
+        st.close();
+        con.close();
+        return teacher;
+    }
 }
