978d8bffabb3ec346391e3b616048040f04b5a12
s84923 <you.sou1108@gmail.com>
2024-04-25 12:23:44 +0900

    a

--- src/main/java/in_out/LogoutAction.java
+++ src/main/java/in_out/LogoutAction.java
@@ -4,18 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import tool.Action;
- 
+
 public class LogoutAction extends Action {
-	public String execute(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {  
-		HttpSession session=request.getSession();
-		
-			if(session.getAttribute("customer")!=null) {
-				session.removeAttribute("customer");
-				return"logout-out.jsp";
-			}
-			
-			return "logout-error.jsp";
-	}
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        session.invalidate(); // セッションを無効化してログアウト
+
+        return "logout.jsp"; // ログアウト画面にリダイレクト
+    }
 }
--- src/main/webapp/in_out/logout-in.jsp
+++
@@ -1,8 +0,0 @@
-<%@ page contentType="text/html; charset=UTF-8" %>
-<%@include file="../header.html" %>
-<form action="Login.action" method="post">
-<p>ログアウトしますか？</p>
-<p><a href="Logout.action">ログアウト</a></p>
-</form>
-
-<%@include file="../footer.html" %>
